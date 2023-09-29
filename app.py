9718339d9490cf213f9ba499024fd5abcc2a92f2
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2023-09-29 13:21:07 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,62 @@
+import os
+import streamlit as st
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+from PIL import Image
+import time
+import glob
+
+
+
+from gtts import gTTS
+from googletrans import Translator
+
+
+st.title("Interfaces Multimodales")
+st.subheader("TRADUCTOR")
+
+
+image = Image.open('traductor.jpg')
+
+st.image(image)
+
+
+st.write("Toca el Botón y habla lo que quires traducir")
+
+stt_button = Button(label=" Inicio ", width=200)
+
+stt_button.js_on_event("button_click", CustomJS(code="""
+    var recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+ 
+    recognition.onresult = function (e) {
+        var value = "";
+        for (var i = e.resultIndex; i < e.results.length; ++i) {
+            if (e.results[i].isFinal) {
+                value += e.results[i][0].transcript;
+            }
+        }
+        if ( value != "") {
+            document.dispatchEvent(new CustomEvent("GET_TEXT", {detail: value}));
+        }
+    }
+    recognition.start();
+    """))
+
+result = streamlit_bokeh_events(
+    stt_button,
+    events="GET_TEXT",
+    key="listen",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+if result:
+    if "GET_TEXT" in result:
+        st.write(result.get("GET_TEXT"))
+    try:
+        os.mkdir("temp")
+    except:
+        pass
